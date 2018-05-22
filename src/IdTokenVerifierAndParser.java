3cbeee2825fd2c4c9763074c116d27927d63fcb0
Gabriel Fernandes <fernandesbgabriel@gmail.com>
2018-05-22 00:19:59 -0300

    Testes de inclusão

---
+++ src/IdTokenVerifierAndParser.java
@@ -0,0 +1,31 @@
+
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson.JacksonFactory;
+
+public class IdTokenVerifierAndParser {
+    private static final String GOOGLE_CLIENT_ID = "851996247372-u2aip6sbfibgd6u7q79vn814j8spce7q.apps.googleusercontent.com";
+
+    public static GoogleIdToken.Payload getPayload (String tokenString) throws Exception {
+
+        JacksonFactory jacksonFactory = new JacksonFactory();
+        GoogleIdTokenVerifier googleIdTokenVerifier =
+                            new GoogleIdTokenVerifier(new NetHttpTransport(), jacksonFactory);
+
+        GoogleIdToken token = GoogleIdToken.parse(jacksonFactory, tokenString);
+
+        if (googleIdTokenVerifier.verify(token)) {
+            GoogleIdToken.Payload payload = token.getPayload();
+            if (!GOOGLE_CLIENT_ID.equals(payload.getAudience())) {
+                throw new IllegalArgumentException("Audience mismatch");
+            } else if (!GOOGLE_CLIENT_ID.equals(payload.getAuthorizedParty())) {
+                throw new IllegalArgumentException("Client ID mismatch");
+            }
+            return payload;
+        } else {
+            throw new IllegalArgumentException("id token cannot be verified");
+        }
+    }
+}
